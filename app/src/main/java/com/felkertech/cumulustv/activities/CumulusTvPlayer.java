43194e9801b554c255e122999e1dfe845fde86d6
Nick <handnf@gmail.com>
2016-12-18 00:48:48 -0500

    Dynamic app shortcuts

--- app/src/main/java/com/felkertech/cumulustv/activities/CumulusTvPlayer.java
+++ app/src/main/java/com/felkertech/cumulustv/activities/CumulusTvPlayer.java
@@ -1,7 +1,12 @@
 package com.felkertech.cumulustv.activities;
 
 import android.content.Intent;
+import android.content.pm.ShortcutInfo;
+import android.content.pm.ShortcutManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Icon;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -12,12 +17,18 @@ import android.widget.MediaController;
 import android.widget.Toast;
 import android.widget.VideoView;
 
+import com.bumptech.glide.Glide;
 import com.felkertech.channelsurfer.players.TvInputPlayer;
 import com.felkertech.channelsurfer.players.WebInputPlayer;
+import com.felkertech.cumulustv.model.ChannelDatabase;
+import com.felkertech.cumulustv.model.JsonChannel;
 import com.felkertech.n.cumulustv.R;
 import com.google.android.exoplayer.ExoPlaybackException;
+import com.squareup.picasso.Picasso;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.Arrays;
 
 /**
  * Created by Nick on 7/12/2015.
@@ -26,7 +37,6 @@ import java.net.URL;
 public class CumulusTvPlayer extends AppCompatActivity {
     private String urlStream;
     private VideoView myVideoView;
-    private URL url;
     private String TAG = "cumulus:CumulusTvPlayer";
     public static final String KEY_VIDEO_URL = "VIDEO_URL";
     private TvInputPlayer exoPlayer;
@@ -58,8 +68,11 @@ public class CumulusTvPlayer extends AppCompatActivity {
         } else {
             getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
             getSupportActionBar().hide();
-            final String url = parameters.getStringExtra(KEY_VIDEO_URL);
-            if(!url.isEmpty()) {
+
+            updateLauncherShortcut();
+
+            urlStream = parameters.getStringExtra(KEY_VIDEO_URL);
+            if(!urlStream.isEmpty()) {
                 setContentView(R.layout.full_surfaceview);
                 SurfaceView sv = (SurfaceView) findViewById(R.id.surface);
                 exoPlayer = new TvInputPlayer();
@@ -94,7 +107,7 @@ public class CumulusTvPlayer extends AppCompatActivity {
                                     //Don't do anything
                                 }
                             });
-                            wv.load(url);
+                            wv.load(urlStream);
                             setContentView(wv);
                         }
                     }
@@ -110,7 +123,7 @@ public class CumulusTvPlayer extends AppCompatActivity {
                     }
                 });
                 try {
-                    exoPlayer.prepare(getApplicationContext(), Uri.parse(url), TvInputPlayer.SOURCE_TYPE_HLS);
+                    exoPlayer.prepare(getApplicationContext(), Uri.parse(urlStream), TvInputPlayer.SOURCE_TYPE_HLS);
                 } catch(Exception e) {
 
                 }
@@ -124,5 +137,39 @@ public class CumulusTvPlayer extends AppCompatActivity {
         super.onStop();
         exoPlayer.stop();
         exoPlayer.release();
+    }
+
+    private void updateLauncherShortcut() {
+        // We will have one dynamic shortcut - to whichever stream was played last
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1) {
+                    JsonChannel jsonChannel = ChannelDatabase.getInstance(getApplicationContext())
+                            .findChannelByMediaUrl(urlStream);
+                    Log.d(TAG, "Adding dynamic shortcut to " + jsonChannel.getName());
+                    String logo = ChannelDatabase.getNonNullChannelLogo(jsonChannel);
+                    try {
+                        Bitmap logoBitmap = Picasso.with(getApplicationContext())
+                                .load(logo)
+                                .get();
+                        ShortcutManager shortcutManager = getSystemService(ShortcutManager.class);
+                        shortcutManager.removeAllDynamicShortcuts();
+                        Intent playVideo = new Intent(getApplicationContext(), CumulusTvPlayer.class);
+                        playVideo.setAction("play");
+                        playVideo.putExtra(KEY_VIDEO_URL, urlStream);
+                        ShortcutInfo shortcut = new ShortcutInfo.Builder(CumulusTvPlayer.this, "id1")
+                                .setShortLabel(jsonChannel.getName())
+                                .setLongLabel(jsonChannel.getNumber() + " - " + jsonChannel.getName())
+                                .setIcon(Icon.createWithBitmap(logoBitmap))
+                                .setIntent(playVideo)
+                                .build();
+                        shortcutManager.setDynamicShortcuts(Arrays.asList(shortcut));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
     }
 }
