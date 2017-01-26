4b1f8fb49308035c2f2760303ffb8aa3e0063763
Nick <handnf@gmail.com>
2017-01-26 18:48:11 -0500

    Updates m3u

--- app/src/main/java/com/felkertech/cumulustv/fileio/M3uParser.java
+++ app/src/main/java/com/felkertech/cumulustv/fileio/M3uParser.java
@@ -80,12 +80,15 @@ public class M3uParser {
         Log.d(TAG, "First line is " + line);
         if (line.startsWith("#EXTM3U")) {
             // Parse global attributes
+        } else if (line.startsWith("#EXTINF")) {
+            // This file has no header. It does have channels. We need to reset our read.
         } else {
             // Not an M3u playlist
             return null;
         }
 
         while ((line = in.readLine()) != null) {
+            Log.d(TAG, "Next line: " + line);
             if (line.startsWith("#EXTINF:")) { // This is a channel
                 M3uTvChannel channel = new M3uTvChannel();
                 String channelAttributes = line.substring(0, getLastComma(line));
